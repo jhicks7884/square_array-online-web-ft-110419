29fd5d78d31583537308dd18f639122200bd4cea
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-06 18:44:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
-  new_numbers = []
-  end 	  numbers.each do |num|
-      new_numbers << (num ** 2)
-    end
-    return new_numbers
+  my_array = []
+  array.each do |n|
+    my_value = n**2
+    my_array << my_value
   end
+
+  my_array
 end
